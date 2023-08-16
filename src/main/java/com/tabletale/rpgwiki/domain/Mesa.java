b9ca42e5c624085b9b6fd615caa9da9ecff36a5a
Moab-Alves <moab.alves@dcx.ufpb.br>
2023-08-16 14:35:57 -0300

    First commit

---
+++ src/main/java/com/tabletale/rpgwiki/domain/Mesa.java
@@ -0,0 +1,13 @@
+package com.tabletale.rpgwiki.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "MESAS")
+public class Mesa extends AbstractEntity<Long>{
+
+
+
+
+}
